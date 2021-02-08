b61dbec26713ee19c26d98db0ca86033a049dd01
do.ockhyun <do.ockhyun@joins.com>
2021-02-08 11:27:37 +0900

    step.3 - MVC list, post

--- site/src/main/java/net/joins/site/controller/IndexController.java
+++ site/src/main/java/net/joins/site/controller/IndexController.java
@@ -1,7 +1,10 @@
 package net.joins.site.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -39,14 +44,50 @@ public class IndexController {
     public String index(Model model) {
         log.info("index");
 
+        List<Post> list = IntStream.range(1, 10)
+        .mapToObj(i -> Post.builder()
+                        .link("post.html")
+                        .title("제목 - " + i)
+                        .subtitle("subtitle - " + i)
+                        .writter("misolab")
+                        .updated("2021-02-08")
+                        .build())
+        .collect(Collectors.toList());
+
+        model.addAttribute("list", list);
         return "index";
+    }
+
+    @Getter
+    @Builder
+    static class Post {
+        String link;
+        String title;
+        String subtitle;
+        String writter;
+        String updated;
+        //  for post page
+        String content;
+        String bgImage;
     }
 
     @GetMapping("post.html")
     public String post(Model model) {
         log.info("post");
 
+        Post post = Post.builder()
+                        .title("Man must explore, and")
+                        .subtitle("Problems look mighty small from 150 miles up")
+                        .writter("misolab")
+                        .updated("2021-02-08")
+                        .content("Wellcome!!<img class=\"img-fluid\" src=\"img/post-sample-image.jpg\">")
+                        .bgImage("img/post-bg.jpg")
+                        .build();
+
+        model.addAttribute("post", post);
         return "post";
     }
+
+    
 
 }
