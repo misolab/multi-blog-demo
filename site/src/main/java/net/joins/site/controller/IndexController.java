974f9e9b0b74794a99c14ac16a80bd568de2e258
do.ockhyun <do.ockhyun@joins.com>
2021-02-08 10:54:14 +0900

    step.3 - MVC 공통

--- site/src/main/java/net/joins/site/controller/IndexController.java
+++ site/src/main/java/net/joins/site/controller/IndexController.java
@@ -1,8 +1,12 @@
 package net.joins.site.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
@@ -14,10 +18,27 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/")
 public class IndexController {
 
+    @ModelAttribute
+    public void common(Model model) {
+        Map<String, String > header = new HashMap<>();
+        header.put("name", "misolab.com");
+        header.put("url", "https://www.misolab.com");
+        header.put("title", "Misolab's Blog");
+        header.put("subtitle", "developed by multi-archetype");
+
+        Map<String, String > footer = new HashMap<>();
+        footer.put("github", "https://www.github.com");
+        footer.put("twitter", "https://www.twitter.com");
+        footer.put("facebook", "https://www.facebook.com");
+        
+        model.addAttribute("header", header);
+        model.addAttribute("footer", footer);
+    }
+
     @GetMapping("index.html")
     public String index(Model model) {
         log.info("index");
-        
+
         return "index";
     }
 
